e9e4c2efad0daa63059f4e318d5d990bcb1ef886
jaymie9019 <jjj19929019@outlook.com>
2023-05-03 15:30:13 +0800

    update conf

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -102,7 +102,7 @@ async function resolveUpdater() {
   // 更新 update.json 文件
   const { data: updateRelease } = await github.rest.repos.getReleaseByTag({
     ...options,
-    tag: tag.name,
+    tag: UPDATE_TAG_NAME,
   });
 
   for (let asset of updateRelease.assets) {
